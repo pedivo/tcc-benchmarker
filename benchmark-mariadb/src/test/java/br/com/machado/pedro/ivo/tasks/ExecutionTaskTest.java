bd2a6c3ea42df8cb627c0b6e55c21471bdc6f947
Pedro Ivo Machado <pmachado@meplis.com>
2014-09-14 20:40:00 -0700

    feat(mariadb) mariadbs tests implemented

---
+++ benchmark-mariadb/src/test/java/br/com/machado/pedro/ivo/tasks/ExecutionTaskTest.java
@@ -0,0 +1,17 @@
+package br.com.machado.pedro.ivo.tasks;
+
+import br.com.machado.pedro.ivo.dao.SimpleDAOMariaDB;
+import br.com.machado.pedro.ivo.dao.factory.DAOFactory;
+import org.junit.Test;
+
+public class ExecutionTaskTest {
+
+		@Test
+		public void printEntity() {
+				System.setProperty(DAOFactory.SIMPLE_DAO_FOR_NAME, SimpleDAOMariaDB.class.getCanonicalName());
+				System.setProperty(DAOFactory.DB_CONNECTION_POOL, "75");
+
+				Command task = new ExecutionTask();
+				task.execute();
+		}
+}
---
+++ benchmark-mariadb/src/test/java/br/com/machado/pedro/ivo/tasks/SelectAllWithPaginationByIndexedAttributeTaskHandlerTest.java
@@ -0,0 +1,21 @@
+package br.com.machado.pedro.ivo.tasks;
+
+import br.com.machado.pedro.ivo.dao.SimpleDAOMariaDB;
+import br.com.machado.pedro.ivo.dao.factory.DAOFactory;
+import br.com.machado.pedro.ivo.dao.generic.SimpleDAO;
+import org.junit.Test;
+
+public class SelectAllWithPaginationByIndexedAttributeTaskHandlerTest {
+
+		@Test
+		public void printEntity() {
+				System.setProperty(DAOFactory.SIMPLE_DAO_FOR_NAME, SimpleDAOMariaDB.class.getCanonicalName());
+
+				//Initialize
+				SimpleDAO dao = DAOFactory.getInstance();
+				DAOFactory.requeue(dao);
+
+				Command task = new SelectAllWithPaginationByIndexedAttributeHandlerTask();
+				task.execute();
+		}
+}
